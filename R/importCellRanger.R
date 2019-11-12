d62819bf9c131ed5cea5413feba8399146282921
87875172 <314100793@qq.com>
2019-11-12 14:38:25 -0500

    add wrappers importCellRangerV2 & importCellRangerV3

--- R/importCellRanger.R
+++ R/importCellRanger.R
@@ -152,66 +152,8 @@
 }
 
 
-#' @title Construct SCE object from Cell Ranger output
-#' @description Read the filtered barcodes, features, and matrices for all
-#'  samples from (preferably a single run of) Cell Ranger output. Combine them
-#'  into one big \link[SingleCellExperiment]{SingleCellExperiment} object.
-#' @param cellRangerDirs The root directories where Cell Ranger was run. These
-#'  folders should contain sample specific folders. Default \code{NULL},
-#'  meaning the paths for each sample will be specified in \emph{samples}
-#'  argument.
-#' @param samples Default \code{NULL}. Can be one of
-#' \itemize{
-#'   \item \code{NULL}. All samples within \emph{cellRangerDirs} will be
-#'    imported. The order of samples will be first determined by the order of
-#'    \emph{cellRangerDirs} and then by \link[base]{list.files}. This is only
-#'    for the case where \emph{cellRangerDirs} is specified.
-#'   \item A list of vectors containing sample names to import. Each vector in
-#'    the list corresponds to samples from one of \emph{cellRangerDirs}.
-#'    These names are the same as the folder names under \emph{cellRangerDirs}.
-#'    This is only for the case where \emph{cellRangerDirs} is specified.
-#'   \item A vector of folder paths for the samples to import. This is only for
-#'    the case where \emph{cellRangerDirs} is \code{NULL}.
-#' }
-#' The cells in the final SCE object will be ordered in the same order of
-#' samples.
-#' @param cellRangerOuts Character or character vector for the the intermidiate
-#'  paths to filtered feature count files saved in sparse matrix format for
-#'  each of \emph{cellRangerDirs}.
-#'  Reference genome names might need to be
-#'  appended for CellRanger version below 3.0.0 if reads were mapped to
-#'  multiple genomes when running Cell Ranger pipeline. Default
-#'  \code{"outs/filtered_feature_bc_matrix/"}.
-#' @param matrixFileName Filename for the Market Exchange Format (MEX) sparse
-#'  matrix file. Default \emph{matrix.mtx.gz}.
-#' @param featuresFileName Filename for the feature annotation file. It can be
-#'  \emph{features.tsv.gz} or \emph{genes.tsv}. Default
-#'  \emph{features.tsv.gz}.
-#' @param barcodesFileName Filename for the cell barcode list file. Default
-#'  \emph{barcodes.tsv.gz}.
-#' @param gzipped Boolean. \code{TRUE} if the Cell Ranger output files
-#'  (barcodes.tsv, features.tsv, and matrix.mtx) were
-#'  gzip compressed. \code{FALSE} otherwise. This is true after Cell Ranger
-#'  3.0.0 update. Default \code{TRUE}.
-#' @param class Character. The class of the expression matrix stored in the SCE
-#'  object. Can be one of "DelayedArray" (as returned by
-#'  \link[DelayedArray]{DelayedArray} function), "Matrix" (as returned by
-#'  \link[Matrix]{readMM} function), or "matrix" (as returned by
-#'  \link[base]{matrix} function). Default "DelayedArray".
-#' @return A \code{SingleCellExperiment} object containing the combined count
-#'  matrix, the feature annotations, and the cell annotation.
-#' @examples
-#' # Example #1
-#' # The following filtered feature, cell, and matrix files were downloaded from
-#' # https://support.10xgenomics.com/single-cell-gene-expression/datasets/
-#' # 3.0.0/hgmm_1k_v3
-#' # The top 10 hg19 & mm10 genes are included in this example.
-#' # Only the first 20 cells are included.
-#' sce <- importCellRanger(
-#'     cellRangerDirs = system.file("extdata", package = "scruff"),
-#'     samples = "hgmm_1k_v3_20x20")
-#' @export
-importCellRanger <- function(
+# main function
+.importCellRanger <- function(
     cellRangerDirs = NULL,
     samples = NULL,
     cellRangerOuts = "outs/filtered_feature_bc_matrix/",
@@ -241,4 +183,139 @@ importCellRanger <- function(
 
     sce <- do.call(BiocGenerics::cbind, res)
     return(sce)
+}
+
+
+#' @name importCellRanger
+#' @rdname importCellRanger
+#' @title Construct SCE object from Cell Ranger output
+#' @description Read the filtered barcodes, features, and matrices for all
+#'  samples from (preferably a single run of) Cell Ranger output. Combine them
+#'  into one big \link[SingleCellExperiment]{SingleCellExperiment} object.
+#' @param cellRangerDirs The root directories where Cell Ranger was run. These
+#'  folders should contain sample specific folders. Default \code{NULL},
+#'  meaning the paths for each sample will be specified in \emph{samples}
+#'  argument.
+#' @param samples Default \code{NULL}. Can be one of
+#' \itemize{
+#'   \item \code{NULL}. All samples within \emph{cellRangerDirs} will be
+#'    imported. The order of samples will be first determined by the order of
+#'    \emph{cellRangerDirs} and then by \link[base]{list.files}. This is only
+#'    for the case where \emph{cellRangerDirs} is specified.
+#'   \item A list of vectors containing sample names to import. Each vector in
+#'    the list corresponds to samples from one of \emph{cellRangerDirs}.
+#'    These names are the same as the folder names under \emph{cellRangerDirs}.
+#'    This is only for the case where \emph{cellRangerDirs} is specified.
+#'   \item A vector of folder paths for the samples to import. This is only for
+#'    the case where \emph{cellRangerDirs} is \code{NULL}.
+#' }
+#' The cells in the final SCE object will be ordered in the same order of
+#' samples.
+#' @param cellRangerOuts Character or character vector for the the intermidiate
+#'  paths to filtered feature count files saved in sparse matrix format for
+#'  each of \emph{cellRangerDirs}.
+#'  Reference genome names might need to be
+#'  appended for CellRanger version below 3.0.0 if reads were mapped to
+#'  multiple genomes when running Cell Ranger pipeline.
+#' @param matrixFileName Filename for the Market Exchange Format (MEX) sparse
+#'  matrix file.
+#' @param featuresFileName Filename for the feature annotation file. It can be
+#'  \emph{features.tsv.gz} or \emph{genes.tsv}.
+#' @param barcodesFileName Filename for the cell barcode list file.
+#' @param gzipped Boolean. \code{TRUE} if the Cell Ranger output files
+#'  (barcodes.tsv, features.tsv, and matrix.mtx) were
+#'  gzip compressed. \code{FALSE} otherwise. This is true after Cell Ranger
+#'  3.0.0 update. Default \code{TRUE}.
+#' @param class Character. The class of the expression matrix stored in the SCE
+#'  object. Can be one of "DelayedArray" (as returned by
+#'  \link[DelayedArray]{DelayedArray} function), "Matrix" (as returned by
+#'  \link[Matrix]{readMM} function), or "matrix" (as returned by
+#'  \link[base]{matrix} function). Default "DelayedArray".
+#' @details
+#'  \code{importCellRangerV2} imports output from Cell Ranger V2.
+#'  \code{importCellRangerV3} imports output from Cell Ranger V3. Some implicit
+#'  assumptions are made in these two functions including \code{cellRangerOuts},
+#'  \code{matrixFileName}, \code{featuresFileName}, and \code{barcodesFileName}.
+#'  Alternatively, user can call \code{importCellRanger} to explicitly
+#'  specify \code{cellRangerOuts}, \code{matrixFileName},
+#'  \code{featuresFileName}, and \code{barcodesFileName}.
+#' @return A \code{SingleCellExperiment} object containing the combined count
+#'  matrix, the feature annotations, and the cell annotation.
+#' @examples
+#' # Example #1
+#' # The following filtered feature, cell, and matrix files were downloaded from
+#' # https://support.10xgenomics.com/single-cell-gene-expression/datasets/
+#' # 3.0.0/hgmm_1k_v3
+#' # The top 10 hg19 & mm10 genes are included in this example.
+#' # Only the first 20 cells are included.
+#' sce <- importCellRanger(
+#'     cellRangerDirs = system.file("extdata", package = "scruff"),
+#'     samples = "hgmm_1k_v3_20x20")
+#' @export
+importCellRanger <- function(
+    cellRangerDirs = NULL,
+    samples = NULL,
+    cellRangerOuts = "outs/filtered_feature_bc_matrix/",
+    matrixFileName = "matrix.mtx.gz",
+    featuresFileName = "features.tsv.gz",
+    barcodesFileName = "barcodes.tsv.gz",
+    gzipped = TRUE,
+    class = "DelayedArray") {
+
+    .importCellRanger(cellRangerDirs = cellRangerDirs,
+        samples = samples,
+        cellRangerOuts = cellRangerOuts,
+        matrixFileName = matrixFileName,
+        featuresFileName = featuresFileName,
+        barcodesFileName = barcodesFileName,
+        gzipped = gzipped,
+        class = class)
+}
+
+
+#' @rdname importCellRanger
+#' @export
+importCellRangerV2 <- function(
+    cellRangerDirs = NULL,
+    samples = NULL,
+    matrixFileName = "matrix.mtx",
+    featuresFileName = "genes.tsv",
+    barcodesFileName = "barcodes.tsv",
+    gzipped = TRUE,
+    class = "DelayedArray") {
+
+    .importCellRanger(cellRangerDirs = cellRangerDirs,
+        samples = samples,
+        cellRangerOuts = "outs/filtered_gene_bc_matrices/",
+        matrixFileName = matrixFileName,
+        featuresFileName = featuresFileName,
+        barcodesFileName = barcodesFileName,
+        gzipped = gzipped,
+        class = class)
+}
+
+
+#' @rdname importCellRanger
+#' @examples
+#' sce <- importCellRangerV3(
+#'     cellRangerDirs = system.file("extdata", package = "scruff"),
+#'     samples = "hgmm_1k_v3_20x20")
+#' @export
+importCellRangerV3 <- function(
+    cellRangerDirs = NULL,
+    samples = NULL,
+    matrixFileName = "matrix.mtx.gz",
+    featuresFileName = "features.tsv.gz",
+    barcodesFileName = "barcodes.tsv.gz",
+    gzipped = TRUE,
+    class = "DelayedArray") {
+
+    .importCellRanger(cellRangerDirs = cellRangerDirs,
+        samples = samples,
+        cellRangerOuts = "outs/filtered_feature_bc_matrix/",
+        matrixFileName = matrixFileName,
+        featuresFileName = featuresFileName,
+        barcodesFileName = barcodesFileName,
+        gzipped = gzipped,
+        class = class)
 }
