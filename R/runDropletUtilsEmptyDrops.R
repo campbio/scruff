97127be366b10ae2bdddd0183c015ea28c0d6f0e
87875172 <314100793@qq.com>
2019-12-12 15:19:00 -0500

    add emptydrops

--- R/runDropletUtilsEmptyDrops.R
+++ R/runDropletUtilsEmptyDrops.R
@@ -1,68 +1,55 @@
-#' @title identifies empty droplets using DropletUtils::emptyDrops
-#' @description distinguishes between droplets containing cells and ambient RNA in a droplet-based single-cell RNA sequencing experiment.
-#' @param sce SingleCellExperiment object. Must contain a raw counts matrix.
-#' @param lower Numeric. The lower bound on the total UMI count, at or below which all barcodes are assumed to correspond to empty droplets.
-#' @return SingleCellExperiment object with the emptyDrops output stored in coldata: emptyDroplet_Total, emptyDroplet_LogProb, emptyDroplet_PValue, emptyDroplet_Limited, emptyDroplet_FDR.
+#' @title Identify empty droplets using \link[DropletUtils]{emptyDrops}.
+#' @description Run \link[DropletUtils]{emptyDrops} on the count matrix in the
+#'  provided \link[SingleCellExperiment]{SingleCellExperiment} object.
+#'  Distinguish between droplets containing cells and ambient RNA in a
+#'  droplet-based single-cell RNA sequencing experiment. This function assumes
+#'  the provided
+#'  \link[SingleCellExperiment]{SingleCellExperiment} object contains cells
+#'  from only one sample and any neccesary subsetting of cells happened
+#'  beforehand.
+#' @param sce A \link[SingleCellExperiment]{SingleCellExperiment} object.
+#'  Must contain an unfiltered raw counts matrix.
+#' @param lower Numeric. The lower bound on the total UMI count, at or below
+#'  which all barcodes are assumed to correspond to empty droplets.
+#' @param ... Additional arguments to be passed to
+#'  \link[DropletUtils]{emptyDrops}.
+#' @return A \link[SingleCellExperiment]{SingleCellExperiment} object with the
+#'  \link[DropletUtils]{emptyDrops} output table appended to the
+#'  \link[SingleCellExperiment]{colData} slot. The columns include
+#'  \emph{EmptyDrops_Total}, \emph{EmptyDrops_LogProb},
+#'  \emph{EmptyDrops_PValue}, \emph{EmptyDrops_Limited}, \emph{EmptyDrops_FDR}.
+#'  Please refer to the documentation of \link[DropletUtils]{emptyDrops} for
+#'  details.
 #' @examples
-#' sce <- runDropletUtilsEmptyDrops(sce=sce, lower=100)
-#' @export
+#' # The following unfiltered PBMC_1k_v3 data were downloaded from
+#' # https://support.10xgenomics.com/single-cell-gene-expression/datasets/3.0.0
+#' # /pbmc_1k_v3
+#' # Only the top 10 cells with most counts and the last 10 cells with non-zero
+#' # counts are included in this example.
+#' # This example only serves as an proof of concept and a tutoriol on how to
+#' # run the function. The results should not be
+#' # used for drawing scientific conclusions.
+#' data(emptyDropsSceExample, package = "scruff")
+#' sce <- runDropletUtilsEmptyDrops(sce = emptyDropsSceExample, lower = 100)
 #' @import DropletUtils
-runDropletUtilsEmptyDrops <-
-  function(sce, lower = 10) {
-    # check if required packages aren't installed
-    list.of.packages <- c("DropletUtils", "SingleCellExperiment")
-    new.packages <-
-      list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
-    
-    # install packages if needed
-    if (length(new.packages))
-      install.packages(new.packages)
-    
-    # load the packages
-    library(DropletUtils)
-    library(SingleCellExperiment)
-    
-    sce.matrix <- 
-      tryCatch(expr = {
-        as.matrix(assay(sce))}, 
+#' @export
+runDropletUtilsEmptyDrops <- function(sce, lower = 10, ...) {
+    cts <- tryCatch(expr = {
+        as(SingleCellExperiment::counts(sce), "dgTMatrix")},
         warning = function(w) {
-          print("Warning: ", w)
+            warning("Warning: ", w)
         },
         error = function(e) {
-          stop("Error: sce assay cannot be converted to matrix. \n\r", e)
+            stop("Error: The count matrix in the provided SCE object cannot be",
+                " converted to 'dgTMatrix'.\n", e)
         })
-    if (!is.numeric(sce.matrix)) {
-      stop("Error: sce assay is not numeric")
-    }
-    rm(sce.matrix)
-    
-    # run DropletsUtils::emptyDrops on the count matrix, store the output to the sce@CalData, and return the updated sce.
-    output <-
-      emptyDrops(m = assay(sce),
-                 lower = lower)
-    colnames(output) <-
-      paste0("emptyDroplet_", colnames(output))
-    sce@colData <-
-      cbind(sce@colData, output)
-    
-    return (sce)
-  }
-    
-# test function. Calling runDropletUtilsEmptyDrops on unfiltered pbmc3k data.
-testRunDropletUtilsEmptyDrops <- function() {
-  library(SingleCellExperiment)
-  
-  sce <- readRDS("data/testUnfilteredSce.rds")
-  
-  #calling runDropletUtilsEmptyDrops
-  sce_test <- runDropletUtilsEmptyDrops(sce=sce, lower = 10)
-  
+
+    # run DropletsUtils::emptyDrops on the count matrix, store the output to
+    # colData(sce), and return the updated sce.
+    output <- emptyDrops(m = cts, lower = lower, ...)
+    colnames(output) <- paste0("EmptyDrops_", colnames(output))
+    SingleCellExperiment::colData(sce) <-
+        cbind(SingleCellExperiment::colData(sce), output)
+
+    return(sce)
 }
-
-#allNamesSamples <- sort(unique(sce@colData$sample))
-
-#for (num in allNamesSamples){
-#  sceIdx <- (which(sce@colDatasample == num))
-  
-  
-#}
