05275f5ad0e698e9e2f48004a37412d1055c292f
zhewa <zhe@bu.edu>
2020-02-05 10:01:20 +0800

    fix test

--- tests/testthat/testscruff.R
+++ tests/testthat/testscruff.R
@@ -14,14 +14,6 @@ test_that(
                 package = "scruff")))
         expect_true(
             file.exists(system.file("extdata",
-                "vandenBrink_b1_L001_R1_001.fastq.gz",
-                package = "scruff")))
-        expect_true(
-            file.exists(system.file("extdata",
-                "vandenBrink_b1_L001_R2_001.fastq.gz",
-                package = "scruff")))
-        expect_true(
-            file.exists(system.file("extdata",
                 "GRCm38_MT.fa", package = "scruff")))
         expect_true(
             file.exists(system.file("extdata",
